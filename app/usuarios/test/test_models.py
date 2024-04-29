6bc21d7e522dfa9f77dc28ccebb795c6884c75b3
diegocervera24 <diego.cervera@udc.es>
2024-04-29 21:42:07 +0200

    Correcion test modelo usuarios

--- app/usuarios/test/test_models.py
+++ app/usuarios/test/test_models.py
@@ -6,20 +6,29 @@ class TestModels(TestCase):
     def setUp(self):
         self.usuario = User.objects # Creamos una variable para acceder a la tabla de usuarios de la BD de las pruebas.
 
-    def test_Usuarios(self):
+    def test_usuarios(self):
         nFilas = len(self.usuario.all()) 
         self.assertEqual(nFilas, 0) # Comprobamos que inicialmente la tabla está vacía.
 
-        self.usuario.create(username="pepe", email="pepe@email.com", nombre="Pepe López", is_admin=True) # Creamos al usuario Pepe
-        user_luis = self.usuario.create(username="luis", email="luis@email.com", nombre="Luis García") # Creamos al usuario Luis
+        admins = self.usuario.filter(is_admin=True).count() 
+        self.assertEqual(admins, 0) # Comprobamos que no hay admins
+
+        staffs = self.usuario.filter(is_staff=True).count() 
+        self.assertEqual(staffs, 0) # Comprobamos que no hay staff
+
+        self.usuario.create(username="pepe", email="pepe@email.com", nombre="Pepe López", is_admin=True, is_staff=True) # Creamos al usuario Pepe
+        user_luis = self.usuario.create(username="luis", email="luis@email.com", nombre="Luis García", is_staff=True) # Creamos al usuario Luis
 
         nFilas = len(self.usuario.all())
         self.assertEqual(nFilas, 2) # Comprobamos que la tabla tiene ahora dos filas.
+        
+        admins = self.usuario.filter(is_admin=True).count() 
+        self.assertEqual(admins, 1) # Comprobamos que hay un admin
+
+        staffs = self.usuario.filter(is_staff=True).count() 
+        self.assertEqual(staffs, 2) # Comprobamos que hay 2 staff
 
         user_luis.delete() # Borramos al usuario Luis
 
         nFilas = len(self.usuario.all())
         self.assertEqual(nFilas, 1) # Comprobamos que la tabla tiene ahora una fila.
-
-        admins = self.usuario.filter(is_admin=True).count() 
-        self.assertEqual(admins, 1) # Comprobamos que hay solo 1 admin
